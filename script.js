48241461bd575ffd3f042cb54f959f0a97019bb5
vinayak parashar <vinayakparashar1200@gmail.com>
2021-10-12 14:20:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+// Get Quote From API
+async function getQuote() {
+  loading()
+  const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+  const apiUrl =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl)
+    const data = await response.json()
+    console.log(data)
+
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'pata nahi'
+    } else {
+      authorText.innerText = data.quoteAuthor
+    }
+
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote')
+    } else {
+      quoteText.classList.add('long-quote')
+    }
+
+    quoteText.innerText = data.quoteText
+
+    // Stop Loader, Show Quote
+    complete()
+  } catch (error) {
+    getQuote()
+  }
+}
+// onLoad
+getQuote()
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText
+  const author = authorText.innerText
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+  window.open(twitterUrl, '_blank')
+}
+
+// Show Loading
+function loading() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false
+    loader.hidden = true
+  }
+}
